22198bab9c46065e6acc62ec52cee031fbc0eb9e
Artem Prokhorov <electron3djavadev@gmail.com>
2023-01-26 12:14:53 +0300

    tried to add Json reading

--- src/main/java/com/electron3d/model/config/IslandSimulationConfig.java
+++ src/main/java/com/electron3d/model/config/IslandSimulationConfig.java
@@ -1,23 +1,23 @@
 package com.electron3d.model.config;
 
-import java.util.List;
-
 public class IslandSimulationConfig {
     private static IslandSimulationConfig INSTANCE;
     private static final String SIMULATION_SPEC_FILE_NAME = "simulationCfg.json";
-    private int timeMultiplier;
-    private int maxTimeOfSimulationInSeconds;
-    private final IslandDimensions islandDimensions = new IslandDimensions();
+    private final int timeMultiplier;
+    private final int maxTimeOfSimulationInSeconds;
+    private final IslandDimensions islandDimensions;
 
     public static IslandSimulationConfig getInstance() {
         if (INSTANCE == null) {
-            INSTANCE = new IslandSimulationConfig();
-            INSTANCE.initFieldsFromSourceFile();
+            INSTANCE = ConfigReader.readConfigFromJSON(SIMULATION_SPEC_FILE_NAME);
         }
         return INSTANCE;
     }
 
-    private IslandSimulationConfig() {
+    private IslandSimulationConfig(int timeMultiplier, int maxTimeOfSimulationInSeconds, IslandDimensions islandDimensions) {
+        this.timeMultiplier = timeMultiplier;
+        this.maxTimeOfSimulationInSeconds = maxTimeOfSimulationInSeconds;
+        this.islandDimensions = islandDimensions;
     }
 
     public int getTimeMultiplier() {
@@ -30,24 +30,6 @@ public class IslandSimulationConfig {
 
     public IslandDimensions getIslandDimensions() {
         return islandDimensions;
-    }
-
-    protected void initFieldsFromSourceFile() {
-        List<String> lines = ConfigReader.readLinesFromCsv(SIMULATION_SPEC_FILE_NAME);
-        setupSimulationParameters(lines);
-        setupIslandDimensions(lines);
-    }
-
-    private void setupSimulationParameters(List<String> lines) {
-        //todo
-        maxTimeOfSimulationInSeconds = 150;
-        timeMultiplier = 8;
-    }
-
-    private void setupIslandDimensions(List<String> lines) {
-        //todo
-        islandDimensions.yDimension = 10;
-        islandDimensions.xDimension = 10;
     }
 
     public static class IslandDimensions {
